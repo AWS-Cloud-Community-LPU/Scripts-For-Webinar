0fd9193188348f3fa2f52eb6303ff37f7ce527b2
Garvit Joshi <garvitjoshi9@gmail.com>
2021-04-10 22:46:06 +0530

    Updated Project Structures, added Comments in Python code

--- AWS-Engage/Wordpress.py
+++ AWS-Engage/Wordpress.py
@@ -1,9 +1,14 @@
+# Script for Installing Wordpress and enabling it
+# in AWS EC2.
+
 import os
+
+
 os.system("yum install mysql")
 os.system("amazon-linux-extras install php7.2")
 os.system("yum install httpd")
 os.system("wget https://wordpress.org/latest.tar.gz")
 os.system("tar -xzf latest.tar.gz")
-##open the httpd.conf file and change the overide from none to all
+# open the httpd.conf file and change the overide from none to all
 os.system("systemctl start httpd")
-#os.system("mysql -h testwordpress.czghgf8deqxw.ap-south-1.rds.amazonaws.com -u admin -p")
+# os.system("mysql -h RDS_ENDPOINT -u admin -p")
